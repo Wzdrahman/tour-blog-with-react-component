2325545d30515a378ec5d82f81a1c3bd6589d271
Wzdrahman <wzd.rahman7@gmail.com>
2021-03-05 16:46:47 +0600

    successfully compiled tour blog project

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id,name,info,price,image,removeTour}) => {
+  const [readMore,setRedMore] = useState(false);
+  return <article className='single-tour'> 
+      <img src={image} alt={name}/>
+      <footer>
+    <div className='tour-info'>
+      <h4>{name}</h4>
+      <h4 className='tour-price'>${price} </h4>
+    </div>
+    <p>
+      {readMore ? info : `${info.substring(0, 200)}...`
+      }
+      <button onClick={()=>setRedMore(!readMore)}>
+        {readMore?'Show Less' : 'Read More'}
+      </button>
+      </p>
+      <button className='delete-btn' onClick={()=> removeTour(id)}>Not Interested</button>
+      </footer>
+  </article>;
 };
 
 export default Tour;
